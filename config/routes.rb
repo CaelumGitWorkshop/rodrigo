2738e7874788cb2c1a0a0bb08698280a15e5a7cd
Caelum Infra <infracaelumrj@gmail.com>
2011-10-18 21:00:51 -0200

    uri menor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CadastroDePessoa::Application.routes.draw do
-  resources :pessoas
+  resources :pessoas, :path => "/"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
